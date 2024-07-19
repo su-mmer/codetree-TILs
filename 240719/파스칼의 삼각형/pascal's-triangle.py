f99ba8a3d2452c8b3d585b21c15ef0df4783456c
Hyeonhee Kim <heekim987@gmail.com>
2024-07-19 23:35:34 +0900

    Title: 파스칼의 삼각형, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240719/파스칼의 삼각형/pascal's-triangle.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = [[1 for _ in range(i+1)] for i in range(n)]
+
+for i in range(2, n):
+    for j in range(1, i):
+        arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+
+for row in arr:
+    for elem in row:
+        print(elem, end=' ')
+    print()
