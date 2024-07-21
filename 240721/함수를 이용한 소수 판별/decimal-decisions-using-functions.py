b5900ad96b931c0ee286bc7e5ee07ee1e9d2afc1
Hyeonhee Kim <heekim987@gmail.com>
2024-07-21 21:48:49 +0900

    Title: 함수를 이용한 소수 판별, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240721/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -2,12 +2,15 @@ a, b = map(int, input().split())
 total = 0
 
 def is_prime(n):
+    if n == 1: 
+        return False
+    
     for i in range(2, n):
         if n % i == 0:
             return False
     return True
 
-    
+
 for i in range(a, b+1):
     if is_prime(i):
         total += i
