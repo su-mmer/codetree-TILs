ca62bb196bdf606de0ab47046909060c9b481502
Hyeonhee Kim <heekim987@gmail.com>
2024-07-21 21:39:41 +0900

    Title: 함수를 이용한 369 게임, Time: 162ms, Memory: 25MB, Status: Passed - CodeTree

--- 240721/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240721/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,18 +1,22 @@
 a, b = map(int, input().split())
 cnt = 0
 
-# def is_3_6_9(i):
-#     while True:
+def is_3_6_9(i):
+    while True:
+        quot, remain = i // 10, i % 10
+        if remain == 3 or remain == 6 or remain == 9:
+            return True
+        if quot < 1:
+            return False
+        i = quot
 
-#     return str(i)
+
+def is_multiple(i):
+    return i % 3 == 0
 
 
-# def is_multiple(i):
-#     return i % 3 == 0
+for i in range(a, b+1):
+    if is_3_6_9(i) or is_multiple(i):
+        cnt += 1
 
-
-# for i in range(a, b+1):
-#     if is_3_6_9(i) or is_multiple(i):
-#         cnt += 1
-
-print(9 / 10)
+print(cnt)
