da987e8c65eeac527ab84b0bfea113ebfe6f838a
Hyeonhee Kim <heekim987@gmail.com>
2024-07-23 00:33:37 +0900

    Title: 특정 구간의 합, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240723/특정 구간의 합/sum-of-specific-intervals.py
+++ 240723/특정 구간의 합/sum-of-specific-intervals.py
@@ -2,10 +2,9 @@ n, m = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 
 def plus(a1, a2):
-    global arr
     total = 0
-    for elem in arr[a1:a2 + 1]:
-        total += elem
+    for i in range(a1, a2 + 1):
+        total += arr[i]
     return total
 
 for _ in range(m):
