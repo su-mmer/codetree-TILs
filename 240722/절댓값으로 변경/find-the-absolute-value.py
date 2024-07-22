56a0b5b343f30f055d31abaf6595a9b077943419
Hyeonhee Kim <heekim987@gmail.com>
2024-07-22 23:48:20 +0900

    Title: 절댓값으로 변경, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/절댓값으로 변경/find-the-absolute-value.py
+++ 240722/절댓값으로 변경/find-the-absolute-value.py
@@ -1,11 +1,11 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-def absolute():
+def absolute(myarr):
     for i in range(n):
-        if arr[i] < 0:
-            arr[i] = -arr[i]
+        if myarr[i] < 0:
+            myarr[i] = -myarr[i]
 
-absolute()
+absolute(arr)
 for elem in arr:
     print(elem, end=' ')
