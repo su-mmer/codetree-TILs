d1821b3f36f60db4fda58031ed1d6a5f827621bd
Hyeonhee Kim <heekim987@gmail.com>
2024-07-27 01:12:12 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240727/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+def calculate(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+
+    if n % 2 == 0:
+        return n + calculate(n - 2)
+    else:
+        return n + calculate(n - 2)
+
+    
+print(calculate(n))
