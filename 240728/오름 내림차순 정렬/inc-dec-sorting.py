4f59ebe82ed61945fbcedebc1ce5e60e1deb5e8d
Hyeonhee Kim <heekim987@gmail.com>
2024-07-28 01:28:57 +0900

    Title: 오름 내림차순 정렬, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240728/오름 내림차순 정렬/inc-dec-sorting.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+arr.sort()
+for elem in arr:
+    print(elem, end = ' ')
+print()
+arr.sort(reverse = True)
+for elem in arr:
+    print(elem, end = ' ')
