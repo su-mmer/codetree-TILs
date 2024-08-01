505a0e17ed8189752e76786270bafa68fd6a1e51
Hyeonhee Kim <heekim987@gmail.com>
2024-08-02 01:06:35 +0900

    Title: Date to Date, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240802/Date to Date/date-to-date.py
@@ -0,0 +1,4 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+print((sum(num_of_days[1:m2]) + d2) - (sum(num_of_days[1:m1]) + d1) + 1)
