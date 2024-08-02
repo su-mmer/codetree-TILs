3b49ea68f9f178a17694ef300910787f9b35a9fc
Hyeonhee Kim <heekim987@gmail.com>
2024-08-03 00:23:11 +0900

    Title: 그 요일은, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240803/그 요일은/the-day-of-the-day.py
+++ 240803/그 요일은/the-day-of-the-day.py
@@ -4,10 +4,13 @@ day = input()
 days_of_year = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 weeks = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-between = sum(days_of_year[1:m2]) - sum(days_of_year[1:m1]) + d2 - d1 + 1
-
-# print(between // 7 + 1) if weeks.index(day) <= between % 7 else print(between // 7)
+between = sum(days_of_year[1:m2]) - sum(days_of_year[1:m1]) + d2 - d1
+# print(between)
+# print(weeks.index(day))
 if weeks.index(day) <= between % 7:
     print(between // 7 + 1)
 else:
     print(between // 7)
+
+# print(between // 7)
+# print(between % 7)
