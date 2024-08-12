8b2ac21a0d8e239b7b6b0c630d2b5a7216f5a415
Hyeonhee Kim <heekim987@gmail.com>
2024-08-13 02:34:34 +0900

    Title: 색종이의 총 넓이, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240813/색종이의 총 넓이/the-total-area-of-colored-paper..py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = [[0 for _ in range(201)] for _ in range(201)]
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    for i in range(x, x+8):
+        for j in range(y, y+8):
+            arr[i][j] = 1
+
+count = 0
+for elem in arr:
+    for c in elem:
+        if c == 1:
+            count += 1
+
+print(count)
