cb8e9ede444963862489c4c510f0ef64eb8601c2
Hyeonhee Kim <heekim987@gmail.com>
2024-08-13 02:37:46 +0900

    Title: 색종이의 총 넓이, Time: 123ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240813/색종이의 총 넓이/the-total-area-of-colored-paper..py
+++ 240813/색종이의 총 넓이/the-total-area-of-colored-paper..py
@@ -1,8 +1,12 @@
+OFFSET = 100
 n = int(input())
 arr = [[0 for _ in range(201)] for _ in range(201)]
 
 for _ in range(n):
     x, y = map(int, input().split())
+    if x < 0: x += OFFSET
+    if y < 0: y += OFFSET
+    
     for i in range(x, x+8):
         for j in range(y, y+8):
             arr[i][j] = 1
