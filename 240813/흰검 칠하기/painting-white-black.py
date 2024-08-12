5e1e5eb1fe1e7e46d0ccc4da47ea1b8b2d2fe160
Hyeonhee Kim <heekim987@gmail.com>
2024-08-13 01:28:28 +0900

    Title: 흰검 칠하기, Time: 192ms, Memory: 29MB, Status: Passed - CodeTree

--- 240813/흰검 칠하기/painting-white-black.py
+++ 240813/흰검 칠하기/painting-white-black.py
@@ -1,36 +1,33 @@
 n = int(input())
-arr = [[] for _ in range(200001)]
+arr = [[] for _ in range(100001)]
 p = 0
-# OFFSET=100000
 
 for _ in range(n):
     x, direction = input().split()
     x = int(x)
     if direction == "R":
-        # if p < 0:
-        #     p += OFFSET
-        for i in range(p, p+x):
+        for i in range(p,p+x):
             arr[i] += 'b'
             # print(arr[i])
-        p += x
+        p += (x - 1)
     elif direction == "L":
-        for i in range(p-x, p):
+        for i in range(p-x+1, p +1):
             arr[i] += 'w'
             # print(arr[i])
-        p -= x
+        p -= (x-1)
 
 gray, white, black = 0, 0, 0
-# print(arr[-1])
 for i in range(len(arr)):
-    # print(arr[i])
     if arr[i]==[]:
-        # print(arr[i])
         continue
     elif arr[i].count('w') >= 2 and arr[i].count('b') >= 2:
+        # print(arr[i])
         gray += 1
     elif arr[i][-1] == 'w':
+        # print(arr[i])
         white += 1
     elif arr[i][-1] == 'b':
+        # print(arr[i])
         black += 1
 
 print(white, black, gray)
