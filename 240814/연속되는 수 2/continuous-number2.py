9d551717083312a2633d5d7faf5eb1213996a231
Hyeonhee Kim <heekim987@gmail.com>
2024-08-14 02:04:20 +0900

    Title: 연속되는 수 2, Time: 236ms, Memory: 24MB, Status: Passed - CodeTree

--- 240814/연속되는 수 2/continuous-number2.py
+++ 240814/연속되는 수 2/continuous-number2.py
@@ -1,11 +1,20 @@
 n = int(input())
-arr = [-1]
+arr = []
 for _ in range(n):
     arr.append(int(input()))
 
-count = 0
+index = []
 for i in range(n):
-    if arr[i] == -1 or arr[i-1] != arr[i]:
-        count += 1
-        
-print(count)
+    if i == 0 or arr[i-1] != arr[i]:
+        index.append(i)
+
+# print(index)
+max_count = 0
+if len(index) == 1:
+    max_count = n
+
+for i in range(1, len(index)):
+    if index[i] - index[i-1] > max_count:
+        max_count = index[i] - index[i-1]
+
+print(max_count)
