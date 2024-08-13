7c3077ed852ff3fbe4513146b46d443d6b0a6e56
Hyeonhee Kim <heekim987@gmail.com>
2024-08-14 01:01:12 +0900

    Title: 계속 중첩되는 사각형, Time: 165ms, Memory: 25MB, Status: Passed - CodeTree

--- 240814/계속 중첩되는 사각형/continuously-overlapping-squares.py
+++ 240814/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -1,9 +1,11 @@
+OFFSET = 100
 n = int(input())
 arr = [[0 for _ in range(201)] for _ in range(201)]
 flag = True
 
 for _ in range(n):
     x1, y1, x2, y2 = map(int, input().split())
+    x1, y1, x2, y2 = x1+OFFSET, y1+OFFSET, x2+OFFSET, y2+OFFSET
     color = 'r' if flag else 'b'
     
     for i in range(x1, x2):
