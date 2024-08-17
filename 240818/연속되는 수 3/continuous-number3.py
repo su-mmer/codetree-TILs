3216cd8f81d9361e08978615faa132a8e9d79469
Hyeonhee Kim <heekim987@gmail.com>
2024-08-18 00:07:11 +0900

    Title: 연속되는 수 3, Time: 227ms, Memory: 24MB, Status: Passed - CodeTree

--- 240818/연속되는 수 3/continuous-number3.py
+++ 240818/연속되는 수 3/continuous-number3.py
@@ -1,30 +1,12 @@
 n = int(input())
-arr = []
+arr = [ int(input()) for _ in range(n) ]
+max_num, cnt = 0, 0
 
-for _ in range(n):
-    arr.append(int(input()))
-
-count = 0
-max_num = 0
 for i in range(n):
-    if i == 0:
-        count += 1
-    elif arr[i] < 0:
-        if arr[i-1] < 0:
-            count += 1
-        elif arr[i-1] > 0:
-            if max_num < count:
-                max_num = count
-                count = 1
-    elif arr[i] > 0:
-        if arr[i-1] > 0:
-            count += 1
-        elif arr[i-1] < 0:
-            if max_num < count:
-                max_num = count
-                count = 1
-
-if max_num == 0:
-    max_num = count
+    if i >= 1 and arr[i] * arr[i-1] > 0:
+        cnt += 1
+    else:
+        cnt = 1
+    max_num = max(max_num, cnt)
 
 print(max_num)
