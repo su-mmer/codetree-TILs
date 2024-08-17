29049939f5e6146835f760dd2fcbabec5c0df626
Hyeonhee Kim <heekim987@gmail.com>
2024-08-18 01:41:12 +0900

    Title: 벌금은 누구에게, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240818/벌금은 누구에게/who-will-pay.py
+++ 240818/벌금은 누구에게/who-will-pay.py
@@ -1,16 +1,12 @@
 n, m, k = map(int ,input().split())
-# student = [0] + [i for i in range(1, n+1)]
-penalty = [0] + [k for _ in range(1, n+1)]
+penalty = [0] + [k for _ in range(1, n+1)]  # 벌칙 몇 번 남았는지 count
 
 student = 0
 for i in range(m):
     num = int(input())
-    penalty[num] -= 1
-    if penalty[num] == 0:
+    penalty[num] -= 1  # 벌칙 받는 학생의 number에서 -1
+    if penalty[num] == 0:  # 학생의 남은 횟수가 0이 되면 학생의 number 찾기
         student = (penalty[1:].index(0) + 1)
         break
-        
-if student == 0:
-    print(-1)
-else:
-    print(student)
+
+print(-1) if student == 0 else print(student)
