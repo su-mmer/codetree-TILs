531b3f2d9c147aad52969eee7ca6f97e574b467e
Hyeonhee Kim <heekim987@gmail.com>
2024-08-18 22:00:10 +0900

    Title: 선두를 지켜라 3, Time: 207ms, Memory: 40MB, Status: Passed - CodeTree

--- 240818/선두를 지켜라 3/keep-the-lead-3.py
+++ 240818/선두를 지켜라 3/keep-the-lead-3.py
@@ -18,42 +18,20 @@ for _ in range(m):
         arr_b[time_b] = arr_b[time_b - 1] + v
         time_b += 1
 
-# 조합
-count = 0
-first = ''
-# first = 'a' if arr_a[1] > arr_b[1] else 'b'
+count = 0  # 선두 변경 횟수
+first = ''  # 선두 주자
 for i in range(1, time_a):
-    if arr_a[i] < arr_b[i]:
-        if first != 'b':
-            first = 'b'
-            count += 1
-    elif arr_a[i] == arr_b[i]:
-        if first != 'ab':
-            first = 'ab'
-            count += 1
-    elif arr_a[i] > arr_b[i]:
-        if first != 'a':
-            first = 'a'
-            count += 1
-        
-    # if arr_a[i] == arr_b[i] and arr_a[i-1] != arr_b[i]:
-    #     count += 1
-    #     frist = 'ab'
-    # elif arr_a[i-1] == arr_b[i-1] and arr_a[i] > arr_b[i]:
-    #     count += 1
-    #     first = 'a'
-    # elif arr_a[i-1] == arr_b[i-1] and arr_a[i] < arr_b[i]:
-    #     count += 1
-    #     first = 'b'
-    # elif arr_a[i-1] != arr_a[i] and arr_a[i] < arr_b[i]:
-        
-    # if arr_a[i] <= arr_b[i] and arr_a[i-1] > arr_b[i-1]:
-    #     first = 'b'
-    #     count += 1
-    # else:
-    #     first = 'a'
-    # if time_a[i] != time_a[i-1] or time_b[i] != time_b[i]:
-        # count += 1
-    # print(arr_a[i], arr_b[i])
+    if arr_a[i] < arr_b[i] and first != 'b':
+        # if first != 'b':
+        first = 'b'
+        count += 1
+    elif arr_a[i] == arr_b[i] and first != 'ab':
+        # if first != 'ab':
+        first = 'ab'
+        count += 1
+    elif arr_a[i] > arr_b[i] and first != 'a':
+        # if first != 'a':
+        first = 'a'
+        count += 1
 
 print(count if count > 0 else 1)
