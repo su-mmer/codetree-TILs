5c3abc77e1da6dbf6c382bc2b7064c1d9984a31d
Hyeonhee Kim <heekim987@gmail.com>
2024-08-26 00:55:47 +0900

    Title: 일렬로 서있는 소 2, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240826/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240826/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -5,7 +5,7 @@ num = 0
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
-            if arr[i] < arr[j] and arr[j] < arr[k]:
+            if arr[i] <= arr[j] and arr[j] <= arr[k]:
                 num += 1
 
 print(num)
