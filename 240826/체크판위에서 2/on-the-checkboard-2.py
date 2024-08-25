5c7456dceebfa6aafc05cf09e5c491c776b5a3fa
Hyeonhee Kim <heekim987@gmail.com>
2024-08-26 01:46:39 +0900

    Title: 체크판위에서 2, Time: 112ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240826/체크판위에서 2/on-the-checkboard-2.py
+++ 240826/체크판위에서 2/on-the-checkboard-2.py
@@ -8,6 +8,9 @@ for i in range(r):
             for k in range(i+1, r-1):  # 범위가 (마지막-1)이어야 함
                 for l in range(j+1, c-1):
                     if arr[i][j] != arr[k][l]:  # 두번째와 다른 색 찾음
-                        num += 1
+                        for x in range(k+1, r):
+                            for y in range(l+1, c):  # 네번째와 마지막이 같은 위치여야 함
+                                if arr[x][y] != arr[k][l] and x == r-1 and y == c-1:
+                                    num += 1
 
 print(num)
