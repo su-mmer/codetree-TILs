96188a718c54beb573cf5321995ae167face480a
Hyeonhee Kim <heekim987@gmail.com>
2024-08-30 00:56:01 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240830/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240830/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -6,9 +6,11 @@ n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 min_num = MAX_INT  # 최대 크기를 미리 넣어서 비교하게 함
-d = 0  # distance
+
 for i in range(1, n - 1):
+    d = 0  # distance
     temp = arr[:i] + arr[i+1:n]  # 1번, N번 체크포인트 제외한 배열
+    
     for j in range(n - 2):  # 거리 계산
         d += abs(temp[j][0] - temp[j+1][0]) + abs(temp[j][1] - temp[j+1][1])
     min_num = min(min_num, d)  # 최소값 비교
