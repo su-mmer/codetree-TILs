885eb8e8ac1bd344047c3229c348fa3c2c0589dc
Hyeonhee Kim <heekim987@gmail.com>
2024-10-26 17:41:08 +0900

    Title: 최고의 13위치 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241026/최고의 13위치 2/best-place-of-13-2.py
+++ 241026/최고의 13위치 2/best-place-of-13-2.py
@@ -7,7 +7,7 @@ for i in range(n-1):
     for j in range(n-2):
         cnt1 = arr[i][j] + arr[i][j+1] + arr[i][j+2]  # first mini board
         for m in range(j+3, n-2):  # 같은 줄 check
-            cnt2 = arr[i][m] + arr[m+1] + arr[i][m+2]
+            cnt2 = arr[i][m] + arr[i][m+1] + arr[i][m+2]
             max_cnt = max(max_cnt, cnt1 + cnt2)
         for k in range(i+1, n):  # 아래 줄부터 check
             for l in range(n-2):
