544c2f5676f45613053cfebebbfc1a4522c9298f
Hyeonhee Kim <heekim987@gmail.com>
2024-10-29 01:50:42 +0900

    Title: 바구니 안의 사탕 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241029/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 241029/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,11 +1,11 @@
 n, k = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-bucket = [0] * 101
+bucket = [0] * 201
 for i in range(n):
-    bucket[arr[i][1]] += arr[i][0]
+    bucket[arr[i][1]+50] += arr[i][0]
 
 max_val = 0
-for i in range(1+k, 101-k):
+for i in range(1+k, 201-k):
     sum_val = 0
     for j in range(i-k, i+k+1):
         sum_val += bucket[j]
