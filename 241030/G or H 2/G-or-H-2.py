65ab820e345678414a3a2556044c1de1e6f38423
Hyeonhee Kim <heekim987@gmail.com>
2024-10-30 02:05:17 +0900

    Title: G or H 2, Time: 206ms, Memory: 25MB, Status: Passed - CodeTree

--- 241030/G or H 2/G-or-H-2.py
+++ 241030/G or H 2/G-or-H-2.py
@@ -5,14 +5,14 @@ for i in range(n):
     arr[i][0] = int(arr[i][0])
 
 sorted_arr = sorted(arr)
-
+# print(sorted_arr)
 max_dist = 0
 for i in range(n):
     alpha = sorted_arr[i][1]  # 기준 알파벳
-    for j in range(i+1, n):
+    for j in range(i+1, n+1):
         num_1, num_2, num_3 = 0, 0, 0
         cnt_same_alph, cnt_ban_alpha = 0, 0
-        for k in range(i, j): # G, H가 같은 개수인지 판별
+        for k in range(i, j): # G, H가 같은 개수인지 판별    # 0, 2
             if sorted_arr[k][1] == alpha:
                 num_1 += 1
             elif sorted_arr[k][1] != alpha:
@@ -20,11 +20,13 @@ for i in range(n):
         if num_1 == num_2: 
             cnt_ban_alpha = sorted_arr[j-1][0] - sorted_arr[i][0]
         for k in range(i, j):  # 기준 알파벳이 어디까지 같은지
+            pv = k
             if sorted_arr[k][1] == alpha:
                 num_3 += 1
             else:
-                cnt_same_alph = sorted_arr[k][0] - sorted_arr[i][0]
                 break
+        cnt_same_alph = sorted_arr[pv][0] - sorted_arr[i][0]
+        
         max_dist = max(cnt_ban_alpha, max_dist)
         max_dist = max(cnt_same_alph, max_dist)
 
