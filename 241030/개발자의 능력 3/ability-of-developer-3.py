e72cd28f6f6ae0b5f61b067e1260ee8fa1afceae
Hyeonhee Kim <heekim987@gmail.com>
2024-10-30 02:44:10 +0900

    Title: 개발자의 능력 3, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 241030/개발자의 능력 3/ability-of-developer-3.py
+++ 241030/개발자의 능력 3/ability-of-developer-3.py
@@ -2,14 +2,15 @@ import sys
 arr = list(map(int, input().split()))
 team_a, team_b = 0, 0
 
-ability = 0
+def calculate(x, y, z):
+    team_a = arr[x] + arr[y] + arr[z]
+    team_b = sum(arr) - team_a
+    return abs(team_a - team_b)
+
 min_cost = sys.maxsize
 for x in range(6):
     for y in range(x + 1, 6):
         for z in range(y + 1, 6):
-            team_a = x + y + z
-            team_b = sum(arr) - team_a
-            ability = abs(team_a - team_b)
-        min_cost = min(min_cost, ability)
+            min_cost = min(min_cost, calculate(x, y, z))
 
 print(min_cost)
