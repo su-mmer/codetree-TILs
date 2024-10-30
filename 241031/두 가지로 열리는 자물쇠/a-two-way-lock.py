b3ce2bdc39e33955c163dee156dac12717cd204a
Hyeonhee Kim <heekim987@gmail.com>
2024-10-31 02:15:26 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 241031/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 241031/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -29,18 +29,24 @@ set_a1 = set()
 for x in range(a1-2, a1+3):
     if x <= 0:
         x = n + x
+    elif x > n:
+        x = x - n
     if 0 < x and x <= n:
         set_a1.add(x)
 set_b1 = set()
 for y in range(b1-2, b1+3):
     if y <= 0:
         y = n + y
+    elif y > n:
+        y = y - n
     if 0 < y and y <= n:
         set_b1.add(y)
 set_c1 = set()
 for z in range(c1-2, c1+3):
     if z <= 0:
         z = n + z
+    elif z > n:
+        z = z - n
     if 0 < z and z <= n:
         set_c1.add(z)
 
@@ -48,18 +54,24 @@ set_a2 = set()
 for x in range(a2-2, a2+3):
     if x <= 0:
         x = n + x
+    elif x > n:
+        x = x - n
     if 0 < x and x <= n:
         set_a2.add(x)
 set_b2 = set()
 for y in range(b2-2, b2+3):
     if y <= 0:
         y = n + y
+    elif y > n:
+        y = y - n
     if 0 < y and y <= n:
         set_b2.add(y)
 set_c2 = set()
 for z in range(c2-2, c2+3):
     if z <= 0:
         z = n + z
+    elif z > n:
+        z = z - n
     if 0 < z and z <= n:
         set_c2.add(z)
 
