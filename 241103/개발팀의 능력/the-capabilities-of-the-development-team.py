784186edb0b1ac59956b9dd521d539c24f28d779
Hyeonhee Kim <heekim987@gmail.com>
2024-11-03 17:10:44 +0900

    Title: 개발팀의 능력, Time: 49ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241103/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,28 @@
+import sys
+n = 5
+arr = list(map(int, input().split()))
+
+def calc_diff(one, two, three):
+    max_val = max(one, two)
+    max_val = max(max_val, three)
+    min_val = min(one, two)
+    min_val = min(min_val, three)
+
+    return max_val - min_val
+
+min_val = sys.maxsize
+for i in range(n):
+    for j in range(i+1, n):
+        team_1 = arr[i] + arr[j]
+        for k in range(n):
+            if k == i or k == j:
+                continue
+            team_3 = arr[k]
+            team_2 = sum(arr) - team_1 - team_3
+            # same score => continue
+            if team_1 == team_2 or team_2 == team_3 or team_3 == team_1:
+                continue
+            # not same score => calculate difference
+            min_val = min(calc_diff(team_1, team_2, team_3), min_val)
+
+print(min_val)
