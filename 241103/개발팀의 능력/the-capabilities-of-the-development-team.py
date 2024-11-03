2113d28fd90c32172a0c66537efcf0a39a34a2a3
Hyeonhee Kim <heekim987@gmail.com>
2024-11-03 17:12:40 +0900

    Title: 개발팀의 능력, Time: 42ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241103/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 241103/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -3,6 +3,10 @@ n = 5
 arr = list(map(int, input().split()))
 
 def calc_diff(one, two, three):
+    # same score => continue
+    if team_1 == team_2 or team_2 == team_3 or team_3 == team_1:
+        return -1
+    # not same score => calculate difference
     max_val = max(one, two)
     max_val = max(max_val, three)
     min_val = min(one, two)
@@ -20,8 +24,8 @@ for i in range(n):
             team_3 = arr[k]
             team_2 = sum(arr) - team_1 - team_3
             # same score => continue
-            if team_1 == team_2 or team_2 == team_3 or team_3 == team_1:
-                continue
+            # if team_1 == team_2 or team_2 == team_3 or team_3 == team_1:
+                # continue
             # not same score => calculate difference
             min_val = min(calc_diff(team_1, team_2, team_3), min_val)
 
