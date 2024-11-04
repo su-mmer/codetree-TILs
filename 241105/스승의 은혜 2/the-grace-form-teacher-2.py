d814f28cf8bdd50cdc18369a1809f09bcb2f8f43
Hyeonhee Kim <heekim987@gmail.com>
2024-11-05 01:20:57 +0900

    Title: 스승의 은혜 2, Time: 64ms, Memory: 16MB, Status: Passed - CodeTree

--- 241105/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 241105/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,11 +1,13 @@
 n, budget = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
-arr.sort()
+# arr.sort()
 
 max_cnt = 0
 for i in range(n):
     temp_arr = arr[:]
     temp_arr[i] = arr[i] // 2
+    temp_arr.sort()
+
     sum_val = 0
     for j in range(n):
         # print(temp_arr)
@@ -14,5 +16,7 @@ for i in range(n):
         if sum_val > budget:
             cnt = j
             break
+        elif sum_val == budget and j == n-1:
+            cnt = n
     max_cnt = max(cnt, max_cnt)
 print(max_cnt)
