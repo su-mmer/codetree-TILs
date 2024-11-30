50258e72de826bc2b54edb3083fef6931e63258e
Hyeonhee Kim <heekim987@gmail.com>
2024-11-30 19:44:32 +0900

    Title: 팰린드롬 수 찾기, Time: 58ms, Memory: 17MB, Status: Passed - CodeTree

--- 241130/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ 241130/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -1,11 +1,16 @@
 x, y = tuple(map(int, input().split()))
 
-count = 0
+count = 0  # 팰린드롬 갯수
 for i in range(x, y + 1):
     number = str(i)
+    flag = True
+
     for n in range(len(number) // 2):
         if number[n] != number[-(n + 1)]:
-            continue
+            flag = False  # 다른 값을 찾으면 False로 변경하고 나가기
+            break
+        
+    if flag == True: 
         count += 1
 
 print(count)
