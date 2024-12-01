040fe8f57f00af2a21a459f1e1d06cda5dfdeec1
Hyeonhee Kim <heekim987@gmail.com>
2024-12-01 17:22:07 +0900

    Title: 이상한 폭탄 2, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 241201/이상한 폭탄 2/strange-bomb-2.py
+++ 241201/이상한 폭탄 2/strange-bomb-2.py
@@ -3,8 +3,13 @@ arr = list(int(input()) for _ in range(n))
 
 bomb = -1  # 터지는 폭탄의 번호
 for i in range(n - 3):
-    for j in range(1, k + 1):
-        if arr[i] == arr[i + j]:  # 거리 내에 같은 번호가 있으면 터짐
+    if i+k+1 >= n:
+        last = n
+    else:
+        last = i + k + 1
+
+    for j in range(i + 1, last):
+        if arr[i] == arr[j]:  # 거리 내에 같은 번호가 있으면 터짐
             bomb = max(bomb, arr[i])
             break
 
