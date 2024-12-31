53e3ae2a67d408d8c25c8abf282a1c4c3fd246d0
Hyeonhee Kim <heekim987@gmail.com>
2024-12-31 20:00:38 +0900

    Title: 팀으로 하는 틱택토 2, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241231/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 241231/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,9 +1,13 @@
 arr = [ list(input()) for _ in range(3) ]
 
 cnt = 0
+check = []
 
 def count_set(aset):
-    if len(set(aset)) == 2:
+    aset = set(aset)
+    if len(aset) == 2 and aset not in check:
+        check.append(aset)
+        # print(check)
         return 1
     else:
         return 0
@@ -20,9 +24,10 @@ for row in arr:
     li_a.append(row[0])
     li_b.append(row[1])
     li_c.append(row[2])
-cnt += count_set(li_a)
-cnt += count_set(li_b)
-cnt += count_set(li_c)
+# print(li_a)
+cnt += (count_set(li_a) + count_set(li_b) + count_set(li_c))
+# cnt += count_set(li_b)
+# cnt += count_set(li_c)
 
 # 대각선
 cnt += count_set([arr[0][0], arr[1][1], arr[2][2]])
