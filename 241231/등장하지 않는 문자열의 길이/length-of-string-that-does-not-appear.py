f1c9e47c05c084cd63554e04deb08daa4b6fe801
Hyeonhee Kim <heekim987@gmail.com>
2024-12-31 20:39:21 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 51ms, Memory: 16MB, Status: Passed - CodeTree

--- 241231/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 241231/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -4,11 +4,21 @@ arr = input()
 for i in range(1, n + 1):  # 문자열 길이
     flag = True  # 1번인지 판독
 
-    for j in range(n - i):
+    for j in range(n - i + 1):
+#         print(arr[j:j+i])
         origin = arr[j:j + i]  # 비교할 문자열
-        if arr.count(origin) > 1:
-            flag = False
-            break
+        # print(origin)
+        cnt = 0
+        for k in range(n - i + 1):  # 전체 문자열에서 origin 찾기
+            if origin == arr[k:k+i]:
+                cnt += 1
+            if cnt > 1:
+                flag = False
+                break
+        # print(origin, arr.count(origin))
+        # if arr.count(origin) > 1:
+        #     flag = False
+        #     break
     if flag:  # 한 번만 발견된 문자열의 최소 길이
         ans = i
         break
