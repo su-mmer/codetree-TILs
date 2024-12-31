7e54757373fc233dbe437b8422d9c30b18d218c1
Hyeonhee Kim <heekim987@gmail.com>
2024-12-31 21:46:26 +0900

    Title: 원소 값들의 최대 합, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

--- 241231/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 241231/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -2,20 +2,12 @@ n, m = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 
 max_mv = 0  # 움직임 원소값 합의 최대
-for i in range(1, n):  # 배열 전체 탐색 (시작 위치)
-    cp = arr[:]  # array init
-    mv = cp[i]
+for i in range(1, n + 1):  # 배열 전체 탐색 (시작 위치)
+    mv = arr[i]  # 처음 원소 init
     sum_mv = 0  # 원소값 합 init
-    for j in range(m):  # m번 움직임
-        # print(mv)
-        # mv = cp[i]
-        # print(cp[i], cp[mv], cp[mv], mv)
-        cp[i], cp[mv] = cp[mv], mv  # swap
-        # print(cp[i], cp[mv], cp)
+    for _ in range(m):  # m번 움직임
         sum_mv += mv  # 움직임 원소값 합
-        mv = cp[i]
-        # print(cp)
-    # print(sum_mv)
+        mv = arr[mv]
     max_mv = max(max_mv, sum_mv)  # 최대 비교
 
 print(max_mv)
