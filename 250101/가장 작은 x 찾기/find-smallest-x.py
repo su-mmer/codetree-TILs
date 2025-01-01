1d2f7bd1f91a639bc1c4af1bc4b2ad6f8c0632db
Hyeonhee Kim <heekim987@gmail.com>
2025-01-01 18:32:57 +0900

    Title: 가장 작은 x 찾기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250101/가장 작은 x 찾기/find-smallest-x.py
@@ -0,0 +1,17 @@
+# x로 시작해서 값에 2를 곱하는 것을 n번 반복, x찾기
+n = int(input())
+arr = [ tuple(map(int, input().split())) for _ in range(n)]
+s, e = arr[0][0] // 2, arr[0][1] // 2
+
+for i in range(s, e):
+    x = i
+    flag = True
+    for j in range(n):
+        x *= 2
+        if x < arr[j][0] or arr[j][1] < x:
+            flag = False
+            break
+
+    if flag:
+        print(i)
+        break
