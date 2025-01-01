9aa5049365a04d1c71e6b2b35f80343ddd87d7fe
Hyeonhee Kim <heekim987@gmail.com>
2025-01-01 18:22:28 +0900

    Title: 독서실의 거리두기 5, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250101/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 250101/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,18 +1,16 @@
 n = int(input())
-arr = input()
-arr = ['0'] + list(arr) + ['0']
+arr = list(input())
 
 max_zero = 0
-for i in range(1, n + 1):
-    # print(arr)
-    # if arr[i-1] == '0' and arr[i] == '0' and arr[i+1] == '0':
+for i in range(n):
     if arr[i] == '0':
         arr[i] = '1'  # 자리 init
+        # print(arr.index('1'))
         # print(arr)
         cnt_zero = 0  # 0의 개수
         min_zero = 100  # 가장 가까운 거리
-        for j in range(1, n + 1):
-            if arr[j] == '1' and j != 1:
+        for j in range(arr.index('1') + 1, n):  # 처음 찾은 1의 다음 자리부터 탐색
+            if arr[j] == '1':
                 min_zero = min(min_zero, cnt_zero + 1)
                 cnt_zero = 0
             elif arr[j] == '0':
