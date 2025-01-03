c0d6d201944bea668426c41bdcab81a3849902e6
Hyeonhee Kim <heekim987@gmail.com>
2025-01-03 23:43:22 +0900

    Title: 초기 수열 복원하기, Time: 64ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250103/초기 수열 복원하기/restore-initial-sequence.py
+++ 250103/초기 수열 복원하기/restore-initial-sequence.py
@@ -7,7 +7,7 @@ for i in range(1, n + 1):
     ans += str(init)  # find를 쓰기 위해 str로 변경...
     for j in range(n - 1):  # 원소의 합을 돌면서 원소 추측
         next_var = arr[j] - init  # 다음 값 추측
-        if ans.find(str(next_var)) == -1:  # 처음 나오는 값이면 수열에 추가
+        if ans.find(str(next_var)) == -1 and next_var > 0:  # 처음 나오는 값이면 수열에 추가
             ans += str(next_var)
             init = next_var
         else:  # 이미 있는 값이면 초기화시키고 넘어감
