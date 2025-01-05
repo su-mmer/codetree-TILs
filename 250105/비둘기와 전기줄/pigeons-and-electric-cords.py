5f42ca7405d0c283b9c6ca1f20e93fe4b1437b3c
Hyeonhee Kim <heekim987@gmail.com>
2025-01-05 16:03:41 +0900

    Title: 비둘기와 전기줄, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250105/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 250105/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -1,7 +1,7 @@
 n = int(input())
 arr = [ list(map(int, input().split())) for _ in range(n) ]
-peace = [0] * 10
-where = [False] * 10
+peace = [0] * 11
+where = [False] * 11
 
 cnt = 0
 for p, w in arr:
@@ -9,6 +9,7 @@ for p, w in arr:
         where[p] = True
         peace[p] = w
     elif where[p] and peace[p] != w:  # 방향이 바뀐 구면 비둘기
+        peace[p] = w
         cnt += 1
 
 print(cnt)
