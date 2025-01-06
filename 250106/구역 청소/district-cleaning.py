100b03c8993d78ed75b2317f234f9d9025eaf905
Hyeonhee Kim <heekim987@gmail.com>
2025-01-06 21:41:04 +0900

    Title: 구역 청소, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250106/구역 청소/district-cleaning.py
+++ 250106/구역 청소/district-cleaning.py
@@ -1,5 +1,9 @@
 xa, xb = map(int, input().split())
 xc, xd = map(int, input().split())
 
-arround = (xb - xa + xd - xc) if xb < xc or xd < xa else xb - xa + xd - xc - (xd - xa)
-print(arround)
+if xb <= xc or xd <= xa:
+  print(xb - xa + xd - xc)
+elif xb <= xd:
+  print(xd - xa)
+else:
+  print(xb - xc)
