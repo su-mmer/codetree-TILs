4df352220a4c1cddd9bfc3ab258d8bc5967135e7
Hyeonhee Kim <heekim987@gmail.com>
2025-10-29 01:17:47 +0900

    Title: A/B 출력, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251029/A/B 출력/a-divide-b.py
+++ 251029/A/B 출력/a-divide-b.py
@@ -1,11 +1,11 @@
 a, b = map(int, input().split())
 
 total = str(a // b) + '.'
-tmp = a % b * 10
+tmp = a % b
+
 for _ in range(20):
+    tmp *= 10
     total += str(tmp // b)
-    tmp -= (tmp // b) * b * 10
-    # tmp = (a % b) * 10 // b
-    # total += str(tmp)
+    tmp -= (tmp // b) * b
 
 print(total)
