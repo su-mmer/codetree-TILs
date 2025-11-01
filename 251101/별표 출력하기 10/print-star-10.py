dbd711acc19b59332c2b19c55d463eb6895550d9
Hyeonhee Kim <heekim987@gmail.com>
2025-11-01 19:41:29 +0900

    Title: 별표 출력하기 10, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251101/별표 출력하기 10/print-star-10.py
+++ 251101/별표 출력하기 10/print-star-10.py
@@ -1,6 +1,8 @@
 n = int(input())
 
 cnt_even, cnt_odd = 1, n
+
+# 절반 출력
 for i in range(n):
     if i % 2 == 0:
         for j in range(cnt_even):
@@ -12,15 +14,27 @@ for i in range(n):
         cnt_odd -= 1
     print()
 
-cnt_even -= 1
-cnt_odd += 1
-for i in range(n):
-    if i % 2 == 0:
-        for j in range(cnt_even):
-            print("*", end=' ')
-        cnt_even -= 1
-    else:
-        for j in range(cnt_odd):
-            print("*", end=' ')
-        cnt_odd += 1
-    print()
+# 나머지 절반 출력
+if n % 2 == 0:  # n이 짝수일 경우
+    for i in range(n):
+        if i % 2 == 0:
+            for j in range(cnt_even):
+                print("*", end=' ')
+            cnt_even += 1
+        else:
+            for j in range(cnt_odd):
+                print("*", end=' ')
+            cnt_odd -= 1
+        print()
+else:  # n이 홀수일 경우
+    cnt_even, cnt_odd = cnt_odd, cnt_even 
+    for i in range(n):
+        if i % 2 == 0:
+            for j in range(cnt_even):
+                print("*", end=' ')
+            cnt_even -= 1
+        else:
+            for j in range(cnt_odd):
+                print("*", end=' ')
+            cnt_odd += 1
+        print()
