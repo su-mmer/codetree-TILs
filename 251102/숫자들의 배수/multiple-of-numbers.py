d4fa0124c4d7ff7830dc5bc3b7f830a75b9e97ec
Hyeonhee Kim <heekim987@gmail.com>
2025-11-02 23:12:57 +0900

    Title: 숫자들의 배수, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251102/숫자들의 배수/multiple-of-numbers.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = [ n * x for x in range(1, n * 10) ]
+
+cnt = 0
+for elem in arr:
+    if cnt == 2:
+        break
+    if elem % 5 == 0:
+        cnt += 1
+    print(elem, end=' ')
