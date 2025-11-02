280db5715dac7eaca68061a6a1e6d9dcbe70d7a0
Hyeonhee Kim <heekim987@gmail.com>
2025-11-03 00:41:14 +0900

    Title: 전항의 두 배, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251103/전항의 두 배/twice-the-previous.py
@@ -0,0 +1,8 @@
+arr = [0 for _ in range(10)]
+arr[0], arr[1] = map(int, input().split())
+
+for i in range(2, 10):
+    arr[i] = arr[i-1] + (2 * arr[i-2])
+
+for elem in arr:
+    print(elem, end=' ')
