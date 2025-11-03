2358c2227ba7136d6f5dd686b8d08d9d74f44975
Hyeonhee Kim <heekim987@gmail.com>
2025-11-04 00:50:19 +0900

    Title: 배열 놀이, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251104/배열 놀이/play-with-array.py
+++ 251104/배열 놀이/play-with-array.py
@@ -1,17 +1,22 @@
 n, q = map(int, input().split())
 arr = list(map(int, input().split()))
 
-for i in range(n):
+for _ in range(q):
     q_list = list(map(int, input().split()))
+
     if q_list[0] == 1:
-        print(arr[q_list[1]-1])
+        a = q_list[1]
+        print(arr[a-1])
+
     elif q_list[0] == 2:
-        if q_list[1] not in arr:
+        b = q_list[1]
+        if b not in arr:
             print(0)
         elif arr.count(q_list[1]) == 1:
             print(arr.index(q_list[1]) + 1)
         else:
             print(arr.index(q_list[1]) + 1)
+
     else:
         for j in arr[q_list[1]-1:q_list[2]]:
             print(j, end=' ')
