cbddaca147820ea04dd95be2f94e6efd5a36f1c9
Hyeonhee Kim <heekim987@gmail.com>
2025-11-05 01:15:08 +0900

    Title: N개의 정수 중 최대 2개, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251105/N개의 정수 중 최대 2개/two-max-of-n-num.py
@@ -0,0 +1,20 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+# Please write your code here.
+first, second = a[0], a[0]
+pv = 0
+# 제일 큰 값 찾기
+for i in range(n):
+    if first < a[i]:
+        first = a[i]
+        pv = i
+
+# 두번째 값 찾기
+for i in range(n):
+    if i == pv:  # 첫 번째 값이랑 같은 위치이면 비교하지 않고 패스
+        continue
+    if second < a[i]:
+        second = a[i]
+
+print(first, second, sep=' ')
