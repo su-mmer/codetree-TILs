360f8615259b6e2d86465faa8dbbe063cff37934
Hyeonhee Kim <heekim987@gmail.com>
2025-11-10 02:40:26 +0900

    Title: 함수를 이용한 온전수 판별, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251110/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,17 @@
+def is_answer(n):
+    if n % 2 == 0:
+        return False
+    elif n % 10 == 5:
+        return False
+    elif n % 3 == 0 and n % 9 != 0:
+        return False
+    return True
+
+
+a, b = map(int, input().split())
+cnt = 0
+for i in range(a, b+1):
+    if is_answer(i):
+        cnt += 1
+
+print(cnt)
