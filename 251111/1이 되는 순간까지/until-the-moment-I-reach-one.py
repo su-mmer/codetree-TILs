f50d7d53469d0112fd77b9a4dd99749e134e88f0
Hyeonhee Kim <heekim987@gmail.com>
2025-11-11 01:29:33 +0900

    Title: 1이 되는 순간까지, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251111/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+
+def calculate(n):
+    if n == 1:
+        return 0
+    
+    if n % 2 == 0:
+        return calculate(n // 2) + 1
+    else:
+        return calculate(n // 3) + 1
+
+
+print(calculate(n))
