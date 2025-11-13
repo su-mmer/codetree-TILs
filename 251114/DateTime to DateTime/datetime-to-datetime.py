f87ee9602ed5019b42d09d8cdc619efcb716ada8
Hyeonhee Kim <heekim987@gmail.com>
2025-11-14 02:29:50 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251114/DateTime to DateTime/datetime-to-datetime.py
+++ 251114/DateTime to DateTime/datetime-to-datetime.py
@@ -1,4 +1,4 @@
 a, b, c = map(int, input().split())
 
-# day, hour, minute
-print((a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11))
+time = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11)
+print(-1) if time < 0 else print(time)
