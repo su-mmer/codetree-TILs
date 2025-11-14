4f7839e4c0fa98e4a0533ee1f6cf89aaa5e58230
Hyeonhee Kim <heekim987@gmail.com>
2025-11-15 02:13:41 +0900

    Title: 최대로 겹치는 구간, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251115/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 251115/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -3,11 +3,16 @@ questions = [ list(map(int, input().split())) for _ in range(n) ]
 arr = [ 0 for _ in range(201) ]
 
 for q in questions:
-    if q[0] < 0:
-        q[0] = -1 * q[0] + 100
-        q[1] += 100
-
-    for i in range(q[0], q[1]):
-        arr[i] += 1
+    if q[0] < 0 and q[1] > 0:
+        for i in range(q[1]):
+            arr[i] += 1
+        for i in range(101, abs(q[0])+100+1):
+            arr[i] += 1
+    elif q[0] < 0 and q[1] < 0:
+        for i in range(102, abs(q[0])+100+1):
+            arr[i] += 1
+    else:
+        for i in range(q[0], q[1]):
+            arr[i] += 1
 
 print(max(arr))
