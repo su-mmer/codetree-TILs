593e9e1826d758d68a458f21f02114eea9495e00
Hyeonhee Kim <heekim987@gmail.com>
2026-02-08 19:24:49 +0900

    Title: 최대로 겹치는 구간, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260208/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 260208/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,10 +1,10 @@
 n = int(input())
 orders = [ list(map(int, input().split())) for _ in range(n) ]
-blocks = [ 0 for _ in range(101) ]
+blocks = [ 0 for _ in range(201) ]
 
 for order in orders:
     for o in range(order[0], order[1]):
-        blocks[o] += 1
+        blocks[o+100] += 1
 
     
 print(max(blocks))
