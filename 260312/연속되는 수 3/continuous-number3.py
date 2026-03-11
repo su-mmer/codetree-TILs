e360ca51bdeb306d608bf93d8700cba0a93ffd9d
Hyeonhee Kim <heekim987@gmail.com>
2026-03-12 02:10:13 +0900

    Title: 연속되는 수 3, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 260312/연속되는 수 3/continuous-number3.py
+++ 260312/연속되는 수 3/continuous-number3.py
@@ -2,8 +2,8 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 cnt, ans = 0, 0  # 연속 수열 길이, 최대 길이
-for i in range(1, n):
-    if arr[i] * arr[i-1] > 0:
+for i in range(n):
+    if i >= 1 and arr[i] * arr[i-1] > 0:
         cnt += 1
     else:
         cnt = 1
