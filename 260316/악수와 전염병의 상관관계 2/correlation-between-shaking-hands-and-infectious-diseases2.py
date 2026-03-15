53218b0e96cb45062dc93ebaac876c8a301573af
Hyeonhee Kim <heekim987@gmail.com>
2026-03-16 03:24:57 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 62ms, Memory: 22MB, Status: Wrong Answer - Codetree

--- 260316/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 260316/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -8,15 +8,19 @@ for i in range(t):
     meet[x][time] = y
     meet[y][time] = x
     
-for j in range(1, 251):
+for j in range(1, 30):
     for i in range(1, n+1):
-        # print(meet[meet[i][j]][0])
-        if meet[meet[i][j]][0] == 1 and arr_k[meet[i][j]] < k:
-            meet[i][0] = 1
-            arr_k[meet[i][j]] += 1
+        if meet[i][0] == 1 and arr_k[i] < k:  # 내가 감염자
+            if meet[i][j] != 0 and meet[meet[i][j]][0] == 1:  # 상대 감염자
+                arr_k[i] += 1  # 전염 횟수 +1
+            if meet[i][j] != 0 and meet[meet[i][j]][0] == 0:  # 상대 미감염자
+                arr_k[i] += 1  # 전염 횟수 +1
+                meet[meet[i][j]][0] = 1
+                # print("check: ", meet[i][j])
+                break  # 내가 감염시켰으면 그만
 
 # for elem in meet:
-#     for e in elem[:10]:
+#     for e in elem[:30]:
 #         print(e, end=' ')
 #     print()
 
