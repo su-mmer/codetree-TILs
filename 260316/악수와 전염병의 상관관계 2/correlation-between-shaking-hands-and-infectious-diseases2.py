b5f2533581bf44c57f4942076654ff0ad759f33f
Hyeonhee Kim <heekim987@gmail.com>
2026-03-16 02:32:07 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 57ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260316/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,24 @@
+n, k, p, t = map(int, input().split())
+meet = [[0 for _ in range(250+1)] for _ in range(n+1)]
+meet[p][0] = 1
+arr_k = [0 for _ in range(n+1)]
+
+for i in range(t):
+    time, x, y = map(int, input().split())
+    meet[x][time] = y
+    meet[y][time] = x
+    
+for j in range(1, 251):
+    for i in range(1, n+1):
+        # print(meet[meet[i][j]][0])
+        if meet[meet[i][j]][0] == 1 and arr_k[meet[i][j]] < 2:
+            meet[i][0] = 1
+            arr_k[meet[i][j]] += 1
+
+# for elem in meet:
+#     for e in elem[:10]:
+#         print(e, end=' ')
+#     print()
+
+for i in range(1, n+1):
+    print(meet[i][0], end='')
