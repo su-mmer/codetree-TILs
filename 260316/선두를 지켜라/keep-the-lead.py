aa7c9af8df011a07ed45bb4b710778d3e488f7a2
Hyeonhee Kim <heekim987@gmail.com>
2026-03-16 01:11:10 +0900

    Title: 선두를 지켜라, Time: 102ms, Memory: 33MB, Status: Passed - Codetree

--- 260316/선두를 지켜라/keep-the-lead.py
+++ 260316/선두를 지켜라/keep-the-lead.py
@@ -19,12 +19,20 @@ for _ in range(m):
         arr_b[time] = arr_b[time-1] + v
 
 # 선두 비교
+start = time+1
+for i in range(time+1):
+    if arr_a[i] != arr_b[i]:
+        start = i
+        break
+
+# print(start)
+
 cnt = 0
-for i in range(2, time):
+for i in range(start+1, time+1):
     # print(arr_a[i], arr_b[i])
-    if arr_a[i] < arr_b[i] and arr_a[i-1] >= arr_b[i-1]:
+    if arr_a[i] < arr_b[i] and arr_a[i-1] >= arr_b[i-1]:  # b 선두
         cnt += 1
-    elif arr_b[i] < arr_a[i] and arr_b[i-1] >= arr_a[i-1]:
+    elif arr_b[i] < arr_a[i] and arr_b[i-1] >= arr_a[i-1]:  # a 선두
         cnt += 1
 
 print(cnt)
