fc3bd653c57b4c474d588f150e6a0d5e34ee55f4
Hyeonhee Kim <heekim987@gmail.com>
2026-03-23 23:45:18 +0900

    Title: 되돌아오기 2, Time: 60ms, Memory: 20MB, Status: Passed - Codetree

--- 260323/되돌아오기 2/come-back-2.py
+++ 260323/되돌아오기 2/come-back-2.py
@@ -3,7 +3,7 @@ orders = list(input())
 dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1]
 move_dir = 2
 nx, ny = 0, 0
-time = 0
+time, tag = 0, False
 
 for o in orders:
     if o == 'L':
@@ -16,4 +16,8 @@ for o in orders:
 
     if nx == 0 and ny == 0:
         print(time)
+        tag = True
         break
+
+if tag == False:
+    print(-1)
