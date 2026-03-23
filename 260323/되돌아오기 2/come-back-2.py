1562bec040127ff696dba980eabc1e4d7eff3f55
Hyeonhee Kim <heekim987@gmail.com>
2026-03-23 23:41:15 +0900

    Title: 되돌아오기 2, Time: 59ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260323/되돌아오기 2/come-back-2.py
@@ -0,0 +1,19 @@
+orders = list(input())
+
+dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1]
+move_dir = 2
+nx, ny = 0, 0
+time = 0
+
+for o in orders:
+    if o == 'L':
+        move_dir = (move_dir + 1) % 4 
+    elif o == 'R':
+        move_dir = (move_dir - 1) % 4
+    elif o == 'F':
+        nx, ny = nx + dxs[move_dir], ny + dys[move_dir]
+    time += 1
+
+    if nx == 0 and ny == 0:
+        print(time)
+        break
