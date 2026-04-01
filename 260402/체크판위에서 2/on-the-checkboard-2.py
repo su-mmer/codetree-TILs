2d25d73cbbbd0dd4a16367b79efa088bb459d75a
Hyeonhee Kim <heekim987@gmail.com>
2026-04-02 01:01:29 +0900

    Title: 체크판위에서 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260402/체크판위에서 2/on-the-checkboard-2.py
+++ 260402/체크판위에서 2/on-the-checkboard-2.py
@@ -6,7 +6,7 @@ for i in range(1, r):
     for j in range(1, c):
         for n in range(i + 1, r - 1):
             for m in range(j + 1, c -1):
-                if arr[0][0] != arr[i][j] and arr[i][j] != arr[n][m]:
+                if arr[0][0] != arr[i][j] and arr[i][j] != arr[n][m] and arr[n][m] != arr[r-1][c-1]:
                     cnt += 1
 
 print(cnt)
