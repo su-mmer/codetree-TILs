24096e705f26de4120570aaf6325e621fe6faa40
Hyeonhee Kim <heekim987@gmail.com>
2026-04-02 00:59:40 +0900

    Title: 체크판위에서 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260402/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,12 @@
+r, c = map(int, input().split())
+arr = [list(input().split()) for _ in range(r)]
+
+cnt = 0
+for i in range(1, r):
+    for j in range(1, c):
+        for n in range(i + 1, r - 1):
+            for m in range(j + 1, c -1):
+                if arr[0][0] != arr[i][j] and arr[i][j] != arr[n][m]:
+                    cnt += 1
+
+print(cnt)
