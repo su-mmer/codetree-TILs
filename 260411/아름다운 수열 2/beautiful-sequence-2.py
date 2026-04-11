67abefcb8345d8e3904083ed0d2a1fadd87a173e
Hyeonhee Kim <heekim987@gmail.com>
2026-04-11 23:02:55 +0900

    Title: 아름다운 수열 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260411/아름다운 수열 2/beautiful-sequence-2.py
+++ 260411/아름다운 수열 2/beautiful-sequence-2.py
@@ -9,7 +9,9 @@ for i in range(n - m + 1):
     for k in range(i, i+m):  # arr_a에서 연속 부분 수열
         for j in range(m):  # arr_b의 원소와 비교
             if arr_a[k] == arr_b[j] and check[j] == 0:  # 연속 부분 수열이 b의 원소이고 중복되지 않으면
-                check [j] = 1
+                check[j] = 1
+                break  # 중복방지
+                
     if sum(check) == m:
         cnt += 1
 
