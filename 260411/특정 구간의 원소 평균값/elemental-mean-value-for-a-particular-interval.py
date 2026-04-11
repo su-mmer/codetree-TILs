ced65cf293234d78b95ae5f5e098d81b5f9b79dc
Hyeonhee Kim <heekim987@gmail.com>
2026-04-11 22:28:13 +0900

    Title: 특정 구간의 원소 평균값, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260411/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 260411/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -4,9 +4,10 @@ arr = list(map(int, input().split()))
 cnt = 0
 for i in range(n):
     for j in range(i, n):
-        num = sum(arr[i:j+1]) / len(arr[i:j+1])  # 평균 계산
+        space = arr[i:j+1]
+        avg = sum(space) / len(space)  # 평균 계산
 
-        if num in arr:  # 배열의 원소라면
+        if avg in space:  # 배열의 원소라면
             cnt += 1
 
 print(cnt)
