7faa2f5662f9826accffbc81a1098ea12c3c6955
Hyeonhee Kim <heekim987@gmail.com>
2026-04-15 02:06:09 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260415/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 260415/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -8,15 +8,15 @@ for i in range(1, n+1):
         for k in range(1, n+1):
             flag = False
             # first
-            if (i + a1 >= n and abs(abs(i - a1) - n) <= 2) or abs(i - a1) <= 2:
-                if (j + b1 >= n and abs(abs(j - b1) - n) <= 2) or abs(j - b1) <= 2:
-                    if (k + c1 >= n and abs(abs(k - c1) - n) <= 2) or abs(k - c1) <= 2:
+            if (abs(abs(i - a1) - n) <= 2) or abs(i - a1) <= 2:
+                if (abs(abs(j - b1) - n) <= 2) or abs(j - b1) <= 2:
+                    if (abs(abs(k - c1) - n) <= 2) or abs(k - c1) <= 2:
                         cnt += 1
                         flag = True
             # second
-            if (i + a2 >= n and abs(abs(i - a2) - n) <= 2) or abs(i - a2) <= 2:
-                if (j + b2 >= n and abs(abs(j - b2) - n) <= 2) or abs(j - b2) <= 2:
-                    if (k + c2 >= n and abs(abs(k - c2) - n) <= 2) or abs(k - c2) <= 2:
+            if (abs(abs(i - a2) - n) <= 2) or abs(i - a2) <= 2:
+                if (abs(abs(j - b2) - n) <= 2) or abs(j - b2) <= 2:
+                    if (abs(abs(k - c2) - n) <= 2) or abs(k - c2) <= 2:
                         if flag: continue  # 중복처리
                         cnt += 1
                                                 
